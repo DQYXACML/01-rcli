2461b56875c44586aabc88d19fbd2c12fb1d58fb
Pluto_Dqy <33154059+DQYXACML@users.noreply.github.com>
2024-08-15 14:39:24 +0800

    support csv to json and yaml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use rcli::{process_csv, Opts, Subcommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        Subcommand::Csv(ops) => {
+            let output = if let Some(output) = ops.output {
+                output
+            } else {
+                format!("output.{}", ops.format)
+            };
+            process_csv(&ops.input, output, ops.format)?;
+        }
+    }
+    Ok(())
 }
